ea6eb271bd5ebd63d0ca0f794b374b3abec56c75
Simon Hrabec <simon.hrabec@gmail.com>
2023-05-03 13:55:26 +0900

    add Tree Algorithms - Subordinates

---
+++ tree-algorithms/subordinates/subordinates.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+vector<vector<int>> arr;
+vector<int> res;
+ 
+int dfs(int idx) {
+	int count = 0;
+	for(const int child : arr[idx]) {
+		count += dfs(child) + 1;
+	}
+	res[idx] = count;
+	return count;
+}
+ 
+void solve() {
+	int n;
+	cin >> n;
+	
+	arr.resize(n);
+	res.resize(n);
+ 
+	for(int i = 1; i < n; i++) {
+		int num;
+		cin >> num;
+		arr[num-1].push_back(i);
+	} 
+ 
+	dfs(0);
+ 
+	for(const auto num : res) {
+		cout << num << ' ';
+	}
+	cout << endl;
+}
+ 
+int main(int argc, char const *argv[]) {
+	std::ios_base::sync_with_stdio(false);
+	solve();	
+	return 0;
+}
